bfe27ffad3669c0e0d267221a586d4c91ed19446
xin07 <xin.chen07@ucarinc.com>
2019-07-19 11:47:49 +0800

    commit

---
+++ src/main/java/demo/multiThreadingDemo.java
@@ -0,0 +1,25 @@
+package demo;
+
+class myThread extends Thread{
+    myThread(String name){
+        this.setName(name);
+    }
+    @Override
+    public void run(){
+        System.out.println("ThreadName: " + Thread.currentThread().getName());
+    }
+}
+public class multiThreadingDemo {
+    public static void main(String[] args){
+        myThread mth1 = new myThread("thread-A");
+        myThread mth2 = new myThread("thread-B");
+        myThread mth3 = new myThread("thread-C");
+        myThread mth4 = new myThread("thread-D");
+        myThread mth5 = new myThread("thread-E");
+        mth1.start();
+        mth2.start();
+        mth3.start();
+        mth4.start();
+        mth5.start();
+    }
+}
