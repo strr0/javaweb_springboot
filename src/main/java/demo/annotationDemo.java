98b6c0ec8528e03c369dc3bc458a0b7848c28aa2
cx <xin.chen07@ucarinc.com>
2019-07-24 17:37:04 +0800

    注解Demo

--- src/main/java/demo/annotationDemo.java
+++ src/main/java/demo/annotationDemo.java
@@ -3,6 +3,7 @@ package demo;
 import java.lang.annotation.*;
 import java.util.Arrays;
 
+//注解测试一
 @Inherited
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
@@ -19,16 +20,29 @@ class B extends A{}
 class C{}
 class D extends C{}
 
-public class annotationDemo {
+//注解测试二
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@interface IntegerValue{
+    int value() default 0;
+    String name() default "";
+}
 
+@IntegerValue(value = 20, name = "jack")
+public class annotationDemo {
     public static void main(String[] args) {
-        A instanceA = new B();
+
+        /*A instanceA = new B();
         System.out.println("已使用的@Inherited注解:"+ Arrays.toString(instanceA.getClass().getAnnotations()));
         C instanceC = new D();
-        System.out.println("没有使用的@Inherited注解:"+Arrays.toString(instanceC.getClass().getAnnotations()));
+        System.out.println("没有使用的@Inherited注解:"+Arrays.toString(instanceC.getClass().getAnnotations()));*/
+
+        boolean hasAnnotation = annotationDemo.class.isAnnotationPresent(IntegerValue.class);
+        if ( hasAnnotation ) {
+            IntegerValue testAnnotation = annotationDemo.class.getAnnotation(IntegerValue.class);
+            System.out.println("value:"+testAnnotation.value());
+            System.out.println("name:"+testAnnotation.name());
+        }
     }
-
-
-
 
 }
