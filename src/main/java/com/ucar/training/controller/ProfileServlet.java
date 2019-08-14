d58832b46e20e5c9dea71afa63e6429fff0e271d
cx <xin.chen07@ucarinc.com>
2019-08-14 16:09:19 +0800

    用户信息

--- src/main/java/com/ucar/training/controller/ProfileServlet.java
+++ src/main/java/com/ucar/training/controller/ProfileServlet.java
@@ -2,6 +2,7 @@ package com.ucar.training.controller;
 
 import com.ucar.training.dao.impl.UserDaoImpl;
 import com.ucar.training.entity.User;
+import com.ucar.training.service.impl.UserServiceImpl;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,13 +13,7 @@ import java.io.IOException;
 
 @WebServlet("/ProfileServlet")
 public class ProfileServlet extends HttpServlet {
-
-    @Override
-    public void init() throws ServletException {
-        if(UserDaoImpl.getAdmins() == null){
-            UserDaoImpl.initUserDao();
-        }
-    }
+    private UserServiceImpl impl = new UserServiceImpl();
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -28,7 +23,7 @@ public class ProfileServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String name = request.getParameter("name");
-        User user = UserDaoImpl.getUserByName(name);
+        User user = impl.getUserByName(name);
         request.setAttribute("userKey", user);
         request.getRequestDispatcher("pages/user/profile.jsp").forward(request, response);
     }
