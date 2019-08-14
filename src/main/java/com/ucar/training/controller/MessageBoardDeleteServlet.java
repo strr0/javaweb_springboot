e32f07cbe1286754aa59462f80b9632a732da858
cx <xin.chen07@ucarinc.com>
2019-08-14 16:10:20 +0800

    留言信息删除

--- src/main/java/com/ucar/training/controller/MessageBoardDeleteServlet.java
+++ src/main/java/com/ucar/training/controller/MessageBoardDeleteServlet.java
@@ -1,6 +1,6 @@
 package com.ucar.training.controller;
 
-import com.ucar.training.dao.MessageDAO;
+import com.ucar.training.service.impl.MessageServiceImpl;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,11 +11,13 @@ import java.io.IOException;
 
 @WebServlet("/MessageBoardDeleteServlet")
 public class MessageBoardDeleteServlet extends HttpServlet {
+    private MessageServiceImpl impl = new MessageServiceImpl();
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String id = request.getParameter("id");
-        MessageDAO.deleteMessageBoard(id);
-        this.getServletContext().setAttribute("messageBoardKey", MessageDAO.getMessages());
+        String Id = request.getParameter("id");
+        int id = Integer.parseInt(Id);
+        impl.deleteMessage(id);
         response.getWriter().println("删除成功");
         response.setHeader("refresh", "3,url=MessageBoardServlet");
     }
