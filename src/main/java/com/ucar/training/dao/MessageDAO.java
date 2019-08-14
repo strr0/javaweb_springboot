ed016eff0963f8f6d94c96bdf34533e524a7123e
cx <xin.chen07@ucarinc.com>
2019-08-14 16:06:37 +0800

    message数据库连接接口

--- src/main/java/com/ucar/training/dao/MessageDAO.java
+++ src/main/java/com/ucar/training/dao/MessageDAO.java
@@ -2,32 +2,10 @@ package com.ucar.training.dao;
 
 import com.ucar.training.entity.Message;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class MessageDAO {
-    private static List<Message> messages;
-
-    //初始化
-    public static void initMessageDao(){
-        messages = new ArrayList<>();
-    }
-    //获取message
-    public static List<Message> getMessages() {
-        return messages;
-    }
-    //添加
-    public static void addMessageBoard(Message message){
-        messages.add(message);
-    }
-    //删除
-    public static void deleteMessageBoard(String date){
-        for(int i = 0; i < messages.size(); i++){
-            Message message = messages.get(i);
-            if(date.equals(message.getmDate())){
-                messages.remove(i);
-                return;
-            }
-        }
-    }
+public interface MessageDao {
+    List<Message> getMessages();
+    void addMessageData(String mName, String mData);
+    void deleteMessageData(int id);
 }
