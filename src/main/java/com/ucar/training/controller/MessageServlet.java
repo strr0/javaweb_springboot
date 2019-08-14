028132361fa94ba7158a15a735ef0b9ec56c26b7
cx <xin.chen07@ucarinc.com>
2019-08-14 16:09:48 +0800

    所有用户信息

--- src/main/java/com/ucar/training/controller/MessageServlet.java
+++ src/main/java/com/ucar/training/controller/MessageServlet.java
@@ -1,5 +1,7 @@
 package com.ucar.training.controller;
 
+import com.ucar.training.service.impl.UserServiceImpl;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,8 +11,11 @@ import java.io.IOException;
 
 @WebServlet("/MessageServlet")
 public class MessageServlet extends HttpServlet {
+    private UserServiceImpl impl = new UserServiceImpl();
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("usersKey", impl.getUsers());
         request.getRequestDispatcher("pages/admin/message.jsp").forward(request, response);
     }
 
