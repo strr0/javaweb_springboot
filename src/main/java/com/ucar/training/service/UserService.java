c5c185c79ec98763baa12332d1c6d8ef01af26bb
cx <xin.chen07@ucarinc.com>
2019-08-14 16:02:48 +0800

    service接口

--- src/main/java/com/ucar/training/service/UserService.java
+++ src/main/java/com/ucar/training/service/UserService.java
@@ -1,4 +1,16 @@
 package com.ucar.training.service;
 
+import com.ucar.training.entity.User;
+
+import java.util.List;
+
 public interface UserService {
+    void addUser(String name, String sex, int age, String password, String likes, String tag, int admin);
+    void deleteUser(int id);
+    List<User> getUsers();
+    User matchUser(String name, String password);
+    boolean isExistName(String name);
+    User getUserByName(String name);
+    User getUserById(int id);
+    void updateUser(User user);
 }
