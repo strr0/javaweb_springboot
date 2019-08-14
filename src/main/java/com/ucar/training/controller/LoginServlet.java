52bec073f7cd313b0391f0fca36df8260ea10446
cx <xin.chen07@ucarinc.com>
2019-08-14 16:10:37 +0800

    登录

--- src/main/java/com/ucar/training/controller/LoginServlet.java
+++ src/main/java/com/ucar/training/controller/LoginServlet.java
@@ -2,6 +2,7 @@ package com.ucar.training.controller;
 
 import com.ucar.training.dao.impl.UserDaoImpl;
 import com.ucar.training.entity.User;
+import com.ucar.training.service.impl.UserServiceImpl;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,63 +13,70 @@ import java.io.IOException;
 
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
+    private UserServiceImpl impl = new UserServiceImpl();
 
-    @Override
+    /*@Override
     public void init() throws ServletException {
         if(UserDaoImpl.getAdmins() == null){
             UserDaoImpl.initUserDao();
             this.getServletContext().setAttribute("adminsKey", UserDaoImpl.getAdmins());
             this.getServletContext().setAttribute("usersKey", UserDaoImpl.getUsers());
         }
-    }
+    }*/
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //PrintWriter out = response.getWriter();
         String name = request.getParameter("name");
         String password = request.getParameter("password");
-        //普通用户
-        User user = UserDaoImpl.userMatch(name, password);
+
+        User user = impl.matchUser(name, password);
         if(user != null){
             //request.getSession().setAttribute("adminKey", "no");
             request.getSession().setAttribute("nameKey", name);
             request.getSession().setAttribute("passwordKey", password);
             request.setAttribute("userKey", user);
-            request.getRequestDispatcher("ProfileServlet").forward(request, response);
+
+            if(user.getAdmin() == 0){  //普通用户
+                request.getRequestDispatcher("ProfileServlet").forward(request, response);
+            }
+            else {  //管理员
+                request.getSession().setAttribute("adminKey", "yes");
+                request.setAttribute("usersKey", impl.getUsers());
+                request.getRequestDispatcher("MessageServlet").forward(request, response);
+            }
+
             //out.println("登录成功");
             return;
         }
-        //管理员
-        else if(UserDaoImpl.adminMatch(name, password)){
-            request.getSession().setAttribute("adminKey", "yes");
-            request.getSession().setAttribute("nameKey", name);
-            request.getSession().setAttribute("passwordKey", password);
-            request.getRequestDispatcher("MessageServlet").forward(request, response);
-            return;
+        else {
+            request.setAttribute("message", "用户名或密码不正确");
+            request.getRequestDispatcher("pages/user/login.jsp").forward(request, response);
         }
 
-        request.setAttribute("message", "用户名或密码不正确");
-        request.getRequestDispatcher("pages/user/login.jsp").forward(request, response);
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //判断是否已经登陆
         String name = (String)request.getSession().getAttribute("nameKey");
-        if(name != null){
+
+        if(name == null){
+            request.getRequestDispatcher("pages/user/login.jsp").forward(request, response);
+        }
+        else {
             //判断是否为管理员
             String admin = (String)request.getSession().getAttribute("adminKey");
-            if(admin.equals("yes")){
-                request.getRequestDispatcher("MessageServlet").forward(request, response);
-                return;
-            }
-            else{
-                User user = UserDaoImpl.getUserByName(name);
+            if(admin == null){
+                User user = impl.getUserByName(name);
                 request.setAttribute("userKey", user);
                 request.getRequestDispatcher("pages/user/profile.jsp").forward(request, response);
-                return;
+            }
+            else{
+                request.setAttribute("usersKey", impl.getUsers());
+                request.getRequestDispatcher("MessageServlet").forward(request, response);
             }
         }
-        request.getRequestDispatcher("pages/user/login.jsp").forward(request, response);
+
     }
 }
