f1609dd42b5e5c08e37500cf904bd71c6ed39716
cx <xin.chen07@ucarinc.com>
2019-08-14 16:08:05 +0800

    用户删除

--- src/main/java/com/ucar/training/controller/UserDeleteServlet.java
+++ src/main/java/com/ucar/training/controller/UserDeleteServlet.java
@@ -2,6 +2,7 @@ package com.ucar.training.controller;
 
 import com.ucar.training.dao.impl.UserDaoImpl;
 import com.ucar.training.entity.User;
+import com.ucar.training.service.impl.UserServiceImpl;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,25 +15,26 @@ import java.util.List;
 
 @WebServlet("/UserDeleteServlet")
 public class UserDeleteServlet extends HttpServlet {
+    UserServiceImpl impl = new UserServiceImpl();
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter out = response.getWriter();
 
-        List<User> users = UserDaoImpl.getUsers();
-        if(users == null){
-            out.println("无效删除");
-            return;
-        }
         String admin = (String)request.getSession().getAttribute("adminKey");
-        String nameDelete = request.getParameter("nameDelete");
 
         if(admin.equals("yes")){  //判断是否为管理员操作
-            users = UserDaoImpl.userDelete(nameDelete);
-            this.getServletContext().setAttribute("usersKey", users);
-            out.println("删除成功");
-            out.println("(3s后跳转到message页面)");
-            response.setHeader("refresh", "3;url=MessageServlet");
-
+            String IdDelete = request.getParameter("idDelete");
+            if(IdDelete != null){
+                int idDelete = Integer.parseInt(IdDelete);
+                impl.deleteUser(idDelete);
+                out.println("删除成功");
+                out.println("(3s后跳转到message页面)");
+                response.setHeader("refresh", "3;url=MessageServlet");
+            }
+            else{
+                out.println("无效删除");
+            }
         }
         else{
             out.println("sorry, 你没有权限删除");
