5712236204830b61a80e1c5a6d0a3b143e34b293
cx <xin.chen07@ucarinc.com>
2019-08-16 13:31:22 +0800

    messageMapper注解配置

--- src/main/java/com/ucar/training/mapper/MessageMapper.java
+++ src/main/java/com/ucar/training/mapper/MessageMapper.java
@@ -1,11 +1,20 @@
 package com.ucar.training.mapper;
 
 import com.ucar.training.entity.Message;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 public interface MessageMapper {
+    @Insert("insert into message(m_name, m_data, m_time)" +
+            "values(#{mName}, #{mData}, now())")
     void addMessage(Message message);
+
+    @Delete("delete from message where id = #{id}")
     void deleteMessage(int id);
+
+    @Select("select id as mId, m_name as mName, m_data as mData, m_time as mTime from message")
     List<Message> getMessages();
 }
