8dd2daaa2d33736cae3d59ef3ecbe4293166ba2e
cx <xin.chen07@ucarinc.com>
2019-08-16 10:07:09 +0800

    messageService接口的实现

--- src/main/java/com/ucar/training/service/impl/MessageServiceImpl.java
+++ src/main/java/com/ucar/training/service/impl/MessageServiceImpl.java
@@ -10,17 +10,18 @@ public class MessageServiceImpl implements MessageService {
     private MessageDaoImpl impl = new MessageDaoImpl();
 
     //获取message列表
-    public List<Message> getMessage(){
+    public List<Message> getMessages(){
         return impl.getMessages();
     }
 
     //添加message
     public void addMessage(String name, String data){
-        impl.addMessageData(name, data);
+        Message message = new Message(name, data);
+        impl.insertData(message);
     }
 
     //删除message
     public void deleteMessage(int id){
-        impl.deleteMessageData(id);
+        impl.deleteData(id);
     }
 }
