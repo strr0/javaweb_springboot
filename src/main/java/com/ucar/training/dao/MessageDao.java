3166b787066a578c4acd4774825a3aac3076f21c
cx <xin.chen07@ucarinc.com>
2019-08-16 10:03:13 +0800

    messageDao接口

--- src/main/java/com/ucar/training/dao/MessageDao.java
+++ src/main/java/com/ucar/training/dao/MessageDao.java
@@ -5,7 +5,7 @@ import com.ucar.training.entity.Message;
 import java.util.List;
 
 public interface MessageDao {
+    void insertData(Message message);
+    void deleteData(int id);
     List<Message> getMessages();
-    void addMessageData(String mName, String mData);
-    void deleteMessageData(int id);
 }
